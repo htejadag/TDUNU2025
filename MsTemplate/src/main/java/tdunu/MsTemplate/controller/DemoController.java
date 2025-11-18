cdb3ece48ddac909cde01a65c85db03b4333ce08
Hernando Tejada Gonzales <htejadag25@gmail.com>
2025-11-18 16:43:37 -0500

    Create DemoController.java

---
+++ MsTemplate/src/main/java/tdunu/MsTemplate/controller/DemoController.java
@@ -0,0 +1,16 @@
+package tdunu.MsTemplate.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/demo")
+public class DemoController {
+  
+  @GetMapping(value = "holaMundo")
+  public String holaMundo() {
+    return "Hola Mundo";
+  }
+
+}
