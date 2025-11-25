9dcb895830de69f5ca0b25b2554d6800afef1f2b
SaiChuquipiondo <saichuquipiondopinchi@gmail.com>
2025-11-25 18:17:56 -0500

    commit inicial

---
+++ MsSustentacion/src/test/java/com/proyect/MsSustentacion/MsSustentacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.MsSustentacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSustentacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
