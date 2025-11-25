5f9f49be0cd552fcf38ac2186276855950337785
LesLizeth12 <lespinedo2003@gmail.com>
2025-11-25 18:34:02 -0500

    Creacion de MsPersona - PostgreSQL

---
+++ MsPersona/MsPersona/src/test/java/com/example/MsPersona/MsPersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MsPersona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
