5c637b444c9ceaf97de8e4f0969419a0e16d13e3
BrandonGT13 <frank.10.tulumba@gmail.com>
2025-11-25 19:06:14 -0500

    Correcion dependencia mapper

--- MsEvaluacion/src/main/java/com/example/MsEvaluacion/config/ModelMapperConfig.java
+++ MsEvaluacion/src/main/java/com/example/MsEvaluacion/config/ModelMapperConfig.java
@@ -1,5 +1,6 @@
 package com.example.MsEvaluacion.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfig {
     
     @Bean
-    public ModelMapperConfig modelMapper() {
-        return new ModelMapperConfig();
+    public ModelMapper ModelMapper() {
+        return new ModelMapper();
     }
 }
