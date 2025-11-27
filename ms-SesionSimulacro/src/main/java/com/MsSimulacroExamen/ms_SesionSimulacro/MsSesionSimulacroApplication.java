de531e96a85951f93426ef0c7130f6bca7ebb82c
linderhp0607 <linderhidalgopinedo@gmail.com>
2025-11-27 18:26:23 -0500

    commit-1-LH
    creación de mi MS

---
+++ ms-SesionSimulacro/src/main/java/com/MsSimulacroExamen/ms_SesionSimulacro/MsSesionSimulacroApplication.java
@@ -0,0 +1,13 @@
+package com.MsSimulacroExamen.ms_SesionSimulacro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSesionSimulacroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSesionSimulacroApplication.class, args);
+	}
+
+}
