032b0bc16646db78b841f500637cd5a19cc44b06
EnvYend Ricardo YanHua <ricardoyanhua@gmail.com>
2025-11-27 18:34:15 -0500

    Creación del microservicio GradosTitulos.

---
+++ MsGradosTitulos/src/test/java/com/unu/ms/MsGradosTitulos/MsGradosTitulosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unu.ms.MsGradosTitulos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGradosTitulosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
