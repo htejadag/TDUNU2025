e28eb5ae70aa7f1c673d76d26aa5d0002d43faf8
DiegoCueva <dsaulcuevaramos@gmail.com>
2025-11-28 02:26:59 -0500

    feat: First Commit
    I created tramite's microservice

---
+++ MsTramite/MsTramite/src/test/java/com/example/MsTramite/MsTramiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MsTramite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTramiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
