f128b6c99e085ccd7f38678764c0c3983594490a
Rober Pedro Antonio Riva Marino <rober.riva05@gmail.com>
2025-11-27 22:34:37 -0500

    ultima final finalicita
    CRUD COMPLETO LAS RUTAS CON SWAGGER

--- MsCaja/src/main/java/com/unu/transaccion/config/TransaccionRouter.java
+++ MsCaja/src/main/java/com/unu/transaccion/config/TransaccionRouter.java
@@ -1,8 +1,11 @@
 package com.unu.transaccion.config;
 
 import com.unu.transaccion.handler.TransaccionHandler;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
@@ -12,6 +15,13 @@ import static org.springframework.web.servlet.function.RouterFunctions.route;
 public class TransaccionRouter {
 
     @Bean
+    @RouterOperations({
+            @RouterOperation(path = "/api/transacciones", method = RequestMethod.GET, beanClass = TransaccionHandler.class, beanMethod = "listar"),
+            @RouterOperation(path = "/api/transacciones/{id}", method = RequestMethod.GET, beanClass = TransaccionHandler.class, beanMethod = "obtener"),
+            @RouterOperation(path = "/api/transacciones", method = RequestMethod.POST, beanClass = TransaccionHandler.class, beanMethod = "crear"),
+            @RouterOperation(path = "/api/transacciones/{id}", method = RequestMethod.PUT, beanClass = TransaccionHandler.class, beanMethod = "actualizar"),
+            @RouterOperation(path = "/api/transacciones/{id}", method = RequestMethod.DELETE, beanClass = TransaccionHandler.class, beanMethod = "eliminar")
+    })
     public RouterFunction<ServerResponse> routes(TransaccionHandler handler) {
         return route()
                 .path("/api/transacciones", builder -> builder
