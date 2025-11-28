af6e0274a99e7867020459f9f08ee724f0201845
Ricardo Salazar <daniel19031101@gmail.com>
2025-11-27 22:13:03 -0500

    Borrando config de sergio arcaico
    el swager funciona automaticamente con la librería inteligente

--- MsPosgrado/src/main/java/tdunu/MsPosgrado/constant/ApiConstants.java
+++ MsPosgrado/src/main/java/tdunu/MsPosgrado/constant/ApiConstants.java
@@ -2,14 +2,14 @@ package tdunu.MsPosgrado.constant;
 
 public final class ApiConstants {
 
-    
     private ApiConstants() {}
 
     // --- Rutas Base ---
-    public static final String API_V1 = "/api"; 
+    public static final String API_BASE = "/api"; // Sin versiones, limpio
     
     // --- Rutas de Controladores ---
-    public static final String ESTUDIANTE_PROCESO_URI = API_V1 + "/estudiante-proceso";
+    // Resultado final: "/api/estudiante-proceso"
+    public static final String ESTUDIANTE_PROCESO_URI = API_BASE + "/estudiante-proceso";
     
     // --- Parámetros comunes ---
     public static final String ID_PARAM = "/{id}";
