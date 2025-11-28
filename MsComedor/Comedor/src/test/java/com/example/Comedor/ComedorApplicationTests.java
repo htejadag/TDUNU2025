4457ff22168e30bffecb234cf8783902e2e84d8d
Josman99 <dennisjosue1998@gmail.com>
2025-11-27 22:37:30 -0500

    Mi primer commit y la tarea

---
+++ MsComedor/Comedor/src/test/java/com/example/Comedor/ComedorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Comedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
