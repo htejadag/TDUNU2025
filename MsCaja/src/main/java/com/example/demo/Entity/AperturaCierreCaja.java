90c567657e5a69b084f89577a538030b9fa79766
Té Verde <hikih020@gmail.com>
2025-11-28 03:33:10 +0100

    Entidad Apertura Cierre Caja

--- MsCaja/src/main/java/com/example/demo/Entity/AperturaCierreCaja.java
+++ MsCaja/src/main/java/com/example/demo/Entity/AperturaCierreCaja.java
@@ -1,5 +1,6 @@
 package com.example.demo.Entity;
 
+import java.math.BigDecimal;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
@@ -18,6 +19,20 @@ public class AperturaCierreCaja {
 
     @NotNull(message = "No puede tener idCajero vacío")
     @Positive(message = "El total debe ser mayor que 0")
-    @Column(name = "idCliente", nullable = false)
-    private int idCliente;
+    @Column(name = "idCajero", nullable = false)
+    private int idCajero;
+
+    @Column(name = "fecha", nullable = false)
+    @NotNull(message = "La fecha no puede ser nula")
+    private String fecha;
+
+    @Column(name = "monto_inicial", precision = 8, scale = 2)
+    @NotNull(message = "El monto inicial no puede ser nulo")
+    @DecimalMin(value = "0.0", inclusive = true, message = "El monto inicial debe ser cero o positivo")
+    private BigDecimal montoInicial;
+
+    @Column(name = "monto_final", precision = 8, scale = 2)
+    @DecimalMin(value = "0.0", inclusive = true, message = "El monto final debe ser cero o positivo")
+    private BigDecimal montoFinal;
+
 }
