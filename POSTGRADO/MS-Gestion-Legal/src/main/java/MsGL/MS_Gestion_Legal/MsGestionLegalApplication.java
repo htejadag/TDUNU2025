d0f2974602f875177d14616d66d4f2d274d4c55a
ManuelChirinosBarbaran <109677175+ManuelChirinosBarbaran@users.noreply.github.com>
2025-11-27 19:55:34 -0500

    creacion de Postgrado, y de mi microservicio gestion legal

---
+++ POSTGRADO/MS-Gestion-Legal/src/main/java/MsGL/MS_Gestion_Legal/MsGestionLegalApplication.java
@@ -0,0 +1,13 @@
+package MsGL.MS_Gestion_Legal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestionLegalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGestionLegalApplication.class, args);
+	}
+
+}
