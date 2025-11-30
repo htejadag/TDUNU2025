da65ed349ed6fc5c125f3f534029ed9dae87b257
LesLizeth12 <lespinedo2003@gmail.com>
2025-11-30 11:17:30 -0500

    Creacion MsPlanEstudios - PostgreSQL

---
+++ MsPlanEstudios/src/main/java/com/example/MsPlanEstudios/MsPlanEstudiosApplication.java
@@ -0,0 +1,13 @@
+package com.example.MsPlanEstudios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPlanEstudiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPlanEstudiosApplication.class, args);
+	}
+
+}
