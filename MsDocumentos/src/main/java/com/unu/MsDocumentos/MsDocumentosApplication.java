0a969c7207a51311702ca7937b68f5e7589a54db
jhonatancg <jhonatan.jesuscg@gmail.com>
2025-12-01 03:00:39 -0500

    init MsDocumentos
    Se creo el microservicio de documentos

---
+++ MsDocumentos/src/main/java/com/unu/MsDocumentos/MsDocumentosApplication.java
@@ -0,0 +1,13 @@
+package com.unu.MsDocumentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDocumentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDocumentosApplication.class, args);
+	}
+
+}
