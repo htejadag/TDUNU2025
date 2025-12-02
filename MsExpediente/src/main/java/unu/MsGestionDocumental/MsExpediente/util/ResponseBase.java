8a3ef0cdaa0ddedcb0d2da04ed35e337c5267518
Maria Grandez <marifegrandezunu@gmail.com>
2025-12-01 23:37:08 -0500

    Implementación correcta del responserBase

---
+++ MsExpediente/src/main/java/unu/MsGestionDocumental/MsExpediente/util/ResponseBase.java
@@ -0,0 +1,38 @@
+package unu.MsGestionDocumental.MsExpediente.util;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+
+public class ResponseBase {
+
+  private boolean success;
+  private String message;
+  private T data;
+
+  public static <T> ResponseBase<T> ok(T data) {
+    return ResponseBase.<T>builder()
+        .success(true)
+        .message("Operación exitosa")
+        .data(data)
+        .build();
+  }
+
+  public static <T> ResponseBase<T> ok(String message, T data) {
+    return ResponseBase.<T>builder()
+        .success(true)
+        .message(message)
+        .data(data)
+        .build();
+  }
+
+  public static <T> ResponseBase<T> error(String message) {
+    return ResponseBase.<T>builder()
+        .success(false)
+        .message(message)
+        .build();
+  }
+
+}
