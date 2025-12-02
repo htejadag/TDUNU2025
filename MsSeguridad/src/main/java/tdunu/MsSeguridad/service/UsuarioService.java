bd7c72d5767ba52e7d95fd6e2215ba9c6cd0af8b
RonaldoSol7 <132590908+Ronaldo129@users.noreply.github.com>
2025-12-02 18:49:49 -0500

    Update UsuarioService.java

--- MsSeguridad/src/main/java/tdunu/MsSeguridad/service/UsuarioService.java
+++ MsSeguridad/src/main/java/tdunu/MsSeguridad/service/UsuarioService.java
@@ -1,5 +1,16 @@
 package tdunu.MsSeguridad.service;
 
-public class UsuarioService {
-    
+import tdunu.MsSeguridad.model.request.UsuarioRequest;
+import tdunu.MsSeguridad.model.response.UsuarioResponse;
+
+import java.util.List;
+
+public interface UsuarioService {
+
+    UsuarioResponse guardar(UsuarioRequest request);
+    List<UsuarioResponse> listar();
+    UsuarioResponse obtenerPorId(Long idUsuario);       
+    void eliminar(Long idUsuario);                      
+
 }
+
