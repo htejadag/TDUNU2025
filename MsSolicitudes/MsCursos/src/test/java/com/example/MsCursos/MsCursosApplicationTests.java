078a20fd9ee497c9badaa0477720e4d7b98dcd54
RichardD07 <139718143+RichardD07@users.noreply.github.com>
2025-12-02 17:15:25 -0500

    Add files via upload

---
+++ MsSolicitudes/MsCursos/src/test/java/com/example/MsCursos/MsCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MsCursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
