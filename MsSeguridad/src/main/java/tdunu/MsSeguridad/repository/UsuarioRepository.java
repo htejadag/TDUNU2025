7aafecc3616e3e33ea68b8a05baff3232afe7705
RonaldoSol7 <132590908+Ronaldo129@users.noreply.github.com>
2025-12-03 11:16:24 -0500

    Update UsuarioRepository.java

--- MsSeguridad/src/main/java/tdunu/MsSeguridad/repository/UsuarioRepository.java
+++ MsSeguridad/src/main/java/tdunu/MsSeguridad/repository/UsuarioRepository.java
@@ -2,7 +2,9 @@ package tdunu.MsSeguridad.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import tdunu.MsSeguridad.model.entity.UsuarioModel;
-
+import java.util.Optional;
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
-
+    Optional<UsuarioModel> findByCodUsuario(String codUsuario);
 }
+
+
