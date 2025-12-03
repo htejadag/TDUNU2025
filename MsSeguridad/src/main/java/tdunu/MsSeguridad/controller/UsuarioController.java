f2174754082e4c8824365fc1d5701569a62e8d15
RonaldoSol7 <132590908+Ronaldo129@users.noreply.github.com>
2025-12-02 19:03:19 -0500

    Update UsuarioController.java

--- MsSeguridad/src/main/java/tdunu/MsSeguridad/controller/UsuarioController.java
+++ MsSeguridad/src/main/java/tdunu/MsSeguridad/controller/UsuarioController.java
@@ -1,5 +1,38 @@
 package tdunu.MsSeguridad.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import tdunu.MsSeguridad.model.request.UsuarioRequest;
+import tdunu.MsSeguridad.model.response.UsuarioResponse;
+import tdunu.MsSeguridad.service.UsuarioService;
+import tdunu.MsSeguridad.util.ApiRoutes;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(ApiRoutes.USUARIO.BASE)
+@RequiredArgsConstructor
 public class UsuarioController {
-                    
+
+    private final UsuarioService usuarioService;
+
+    @GetMapping(ApiRoutes.USUARIO.LISTAR)
+    public List<UsuarioResponse> listar() {
+        return usuarioService.listar();
+    }
+
+    @GetMapping(ApiRoutes.USUARIO.OBTENER_POR_ID + "/{id}")
+    public UsuarioResponse obtener(@PathVariable Long id) {
+        return usuarioService.obtenerPorId(id);
+    }
+
+    @PostMapping(ApiRoutes.USUARIO.GUARDAR)
+    public UsuarioResponse guardar(@RequestBody UsuarioRequest request) {
+        return usuarioService.guardar(request);
+    }
+
+    @DeleteMapping(ApiRoutes.USUARIO.ELIMINAR + "/{id}")
+    public void eliminar(@PathVariable Long id) {
+        usuarioService.eliminar(id);
+    }
 }
