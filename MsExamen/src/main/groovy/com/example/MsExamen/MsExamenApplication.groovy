5bb991af10e5d5a294e5460999330b284d7cf9de
roberth <roberthadelmohoyoszuniga@gmail.com>
2025-12-05 09:41:01 -0500

    proyecto inicial

---
+++ MsExamen/src/main/groovy/com/example/MsExamen/MsExamenApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.MsExamen
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MsExamenApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MsExamenApplication, args)
+	}
+
+}
