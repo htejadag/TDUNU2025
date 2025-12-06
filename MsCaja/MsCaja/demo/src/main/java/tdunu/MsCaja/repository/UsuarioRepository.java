eeb9059abad82bd4aba24e2b67d1b105637cc542
AlexAmbrosio1 <alexsebas79@gmail.com>
2025-12-05 21:41:55 -0500

    MsCaja inicializacion
    Se creo MsCaja

--- MsCaja/MsCaja/demo/src/main/java/tdunu/MsCaja/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package tdunu.MsCaja.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tdunu.MsCaja.model.entity.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-}
