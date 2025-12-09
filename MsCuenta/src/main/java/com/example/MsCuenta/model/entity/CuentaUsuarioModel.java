599ddb0296b3b3b8b53ba693749dc308ebe60f7f
AbrhamP <abraham.pacaya0901@gmail.com>
2025-12-09 17:23:19 -0500

    creacion de modelo

---
+++ MsCuenta/src/main/java/com/example/MsCuenta/model/entity/CuentaUsuarioModel.java
@@ -0,0 +1,38 @@
+package com.example.MsCuenta.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "cuenta_usuario")
+
+
+public class CuentaUsuarioModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "id_usuario_rol")
+    private Integer idUsuarioRol;
+    @Column(name = "saldo")
+    private double saldo;
+    @Column (name = "activo")
+    private boolean activo;
+    
+//auditoria
+    @Column(name = "usuario_creacion", nullable = false, length = 100)
+    private Integer usuarioCreacion;
+    @Column(name = "fecha_creacion", nullable = false)
+    private String fechaCreacion;
+    @Column(name = "usuario_modificacion", length = 100)
+    private Integer usuarioModificacion;
+    @Column(name = "fecha_modificacion")
+    private String fechaModificacion;
+
+}
