8bba70cb3443618fddd3089d96596e9a2111e1ec
Dannn <106832279+Danperro@users.noreply.github.com>
2025-12-09 19:13:27 -0500

    doceavo commit

---
+++ MsCursos/src/main/java/com/example/MsCursos/MsCursosApplication.java
@@ -0,0 +1,11 @@
+package com.example.MsCursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCursosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsCursosApplication.class, args);
+    }
+}
