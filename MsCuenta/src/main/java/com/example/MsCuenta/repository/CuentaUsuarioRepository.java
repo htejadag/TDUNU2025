db5dbbd10416306932b30244e12168dc3452f90f
AbrhamP <abraham.pacaya0901@gmail.com>
2025-12-11 12:01:16 -0500

    crud MsCuentaActualizado con Extends

---
+++ MsCuenta/src/main/java/com/example/MsCuenta/repository/CuentaUsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.example.MsCuenta.repository;
+
+import com.example.MsCuenta.model.entity.CuentaUsuarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CuentaUsuarioRepository extends JpaRepository<CuentaUsuarioModel, Integer> {
+    // Aquí puedes añadir métodos personalizados si los necesitas a futuro
+}
