a44b20794bb12b2f485578d47c4d48a51fb46726
ManuelChirinosBarbaran <109677175+ManuelChirinosBarbaran@users.noreply.github.com>
2025-12-11 14:54:12 -0500

    Delete MsBiblioteca directory

--- MsBiblioteca/MsGL/MS_Gestion_Legal/domain/model/EstadoExpediente.java
+++
@@ -1,7 +0,0 @@
-package MsGL.MS_Gestion_Legal.domain.model;
-
-public enum EstadoExpediente {
-     PENDIENTE,
-    OBSERVADO,
-    COMPLETO
-}
--- MsBiblioteca/MsGL/MS_Gestion_Legal/domain/repository/CertificadoRepository.java
+++
@@ -1,6 +0,0 @@
-package MsGL.MS_Gestion_Legal.domain.repository;
-import MsGL.MS_Gestion_Legal.domain.model.Certificado;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface CertificadoRepository extends JpaRepository<Certificado, Long>{
-    
-}
