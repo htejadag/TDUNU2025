5c22672d986220df0d183999ce03f19aa3046a1c
linderhp0607 <linderhidalgopinedo@gmail.com>
2025-12-11 10:13:29 -0500

    commit 2
    update ms

---
+++ MsSimulacro/src/test/java/com/MsSimulacro/MsSimulacro/MsSimulacroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MsSimulacro.MsSimulacro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSimulacroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
