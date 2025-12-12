106b7e1c001f6def2d5dac006481bef1398993f4
ManuelChirinosBarbaran <109677175+ManuelChirinosBarbaran@users.noreply.github.com>
2025-12-11 20:24:16 -0500

    Revert "Delete MsSesionSimulacro directory"
    This reverts commit 1d020bf6ea3fbec61878bab7b1ea275ac4914e8e.

---
+++ MsSesionSimulacro/src/test/java/com/MsSimulacroExamen/ms_SesionSimulacro/MsSesionSimulacroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MsSimulacroExamen.ms_SesionSimulacro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSesionSimulacroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
