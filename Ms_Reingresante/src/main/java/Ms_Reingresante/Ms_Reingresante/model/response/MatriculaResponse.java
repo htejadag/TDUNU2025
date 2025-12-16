b70a490a82c1fbcdbd567adf7f27cacd93d94116
AlmeidaSima <almeida.sima2000@gmail.com>
2025-12-16 16:36:37 -0500

    Matricula de reingreso
    Se ha añadido la matricula de reingreso

---
+++ Ms_Reingresante/src/main/java/Ms_Reingresante/Ms_Reingresante/model/response/MatriculaResponse.java
@@ -0,0 +1,20 @@
+package Ms_Reingresante.Ms_Reingresante.model.response;
+
+import lombok.Data;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+public class MatriculaResponse {
+    private Long idMatricula;
+    private Integer idProceso;
+    private Integer idResolucion;
+    private LocalDate matFecha;
+    private String realizadoPor;
+    
+    // Campos de Auditoría
+    private LocalDateTime fechaCreacion;
+    private String usuarioCreacion;
+    private LocalDateTime fechaModificacion;
+    private String usuarioModificacion;
+}
