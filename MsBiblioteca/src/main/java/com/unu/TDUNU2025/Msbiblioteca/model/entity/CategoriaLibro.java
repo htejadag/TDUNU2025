efc21ef299576a2dbd0a3b5193f1e518aa273096
Jkaiser5 <JKaiser5@gmail.com>
2025-12-29 09:21:52 -0500

    merge ejecutado

---
+++ MsBiblioteca/src/main/java/com/unu/TDUNU2025/Msbiblioteca/model/entity/CategoriaLibro.java
@@ -0,0 +1,46 @@
+package com.unu.TDUNU2025.Msbiblioteca.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "categoria")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoriaLibro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCategoria;
+
+    @Column(nullable = false, unique = true)
+    private String nombre;
+
+    private String descripcion;
+
+    @Column(updatable = false)
+    private LocalDateTime fechaRegistro;
+
+    private LocalDateTime fechaActualizacion;    
+
+    @PrePersist
+    public void prePersist() {
+        fechaRegistro = LocalDateTime.now();
+    }
+    
+    @PreUpdate
+    public void preUpdate(){
+        fechaActualizacion = LocalDateTime.now();
+    }
+
+    // Opcional: Relación inversa (no es estrictamente necesaria para JPA, pero útil)
+    @ManyToMany(mappedBy = "categorias")
+    @ToString.Exclude // Evita StackOverflowError en toString()
+    private Set<Libro> libros;
+
+    
+}
