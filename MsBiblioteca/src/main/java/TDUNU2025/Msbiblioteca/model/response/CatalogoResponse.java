41d12d759edb61a6be7d16e85f3c06235d667f77
Jkaiser5 <JKaiser5@gmail.com>
2025-12-29 14:04:35 -0500

    actualizacion catalogo response

--- MsBiblioteca/src/main/java/TDUNU2025/Msbiblioteca/model/response/CatalogoResponse.java
+++ MsBiblioteca/src/main/java/TDUNU2025/Msbiblioteca/model/response/CatalogoResponse.java
@@ -1,11 +1,16 @@
 package TDUNU2025.Msbiblioteca.model.response;
 
+import java.io.Serializable;
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class CatalogoResponse {
+@NoArgsConstructor
+public class CatalogoResponse implements Serializable {
     private Integer idCatalogo;
     private String nombre;
     private String descripcion;
     private Integer estado;
+    
 }
