3cb5061401c77dbcdf4d8fe5a874a60584491026
xlisden <dayenira.delgado@gmail.com>
2026-01-03 12:18:05 -0500

    fix merge

--- MsAcademico/src/main/java/unu/td/MsAcademico/MsAcademicoApplication.java
+++ MsAcademico/src/main/java/unu/td/MsAcademico/MsAcademicoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class msacademicoApplication {
+public class MsAcademicoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(msacademicoApplication.class, args);
+		SpringApplication.run(MsAcademicoApplication.class, args);
 	}
 
 }
