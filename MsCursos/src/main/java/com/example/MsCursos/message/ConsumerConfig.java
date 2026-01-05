a58843afeaf950134be77bbf628dc8624266e06f
Dannn <106832279+Danperro@users.noreply.github.com>
2026-01-04 23:38:16 -0500

    implementacion de kafka consumer

--- MsCursos/src/main/java/com/example/MsCursos/message/ConsumerConfig.java
+++ MsCursos/src/main/java/com/example/MsCursos/message/ConsumerConfig.java
@@ -1,10 +1,23 @@
 package com.example.MsCursos.message;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 @EnableKafka
 public class ConsumerConfig {
     
+    @Bean
+    public ObjectMapper objectMapper(){
+
+        ObjectMapper om= new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return om;
+    }
 }
---
+++ MsCursos/src/main/java/com/example/MsCursos/message/KafkaConsumerConfig.java
@@ -0,0 +1,46 @@
+package com.example.MsCursos.message;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig  {
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+
+        // ✅ pon tu bootstrap real (o pásalo por properties si prefieres)
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        // ✅ pon tu group-id real
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "ms-cursos-group");
+
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean(name = "kafkaListenerContainerFactory")
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
+            ConsumerFactory<String, String> consumerFactory) {
+
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(consumerFactory);
+        return factory;
+    }
+}
