559f783746e4a21f1d2e60a945b5f959d615dda3
Franco:) <leoc47665@gmail.com>
2026-01-04 19:06:55 -0500

    MsAuditoria base agregado

---
+++ MsAuditoria/src/test/java/com/example/MsAuditoria/MsAuditoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MsAuditoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuditoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
