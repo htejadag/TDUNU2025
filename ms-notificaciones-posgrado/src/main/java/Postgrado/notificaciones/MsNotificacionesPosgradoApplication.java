ba4d147bcfc7cbbe6d0f4faec3005ae94afd3308
Ricardo Salazar <daniel19031101@gmail.com>
2026-01-06 00:09:24 -0500

    add MsNotificacionesPosgrado
    nuevo MS para que sea el consumidor del evento kafka del original que tenemos

---
+++ ms-notificaciones-posgrado/src/main/java/Postgrado/notificaciones/MsNotificacionesPosgradoApplication.java
@@ -0,0 +1,13 @@
+package Postgrado.notificaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotificacionesPosgradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNotificacionesPosgradoApplication.class, args);
+	}
+
+}
---
+++ ms-notificaciones-posgrado/src/test/java/Postgrado/notificaciones/MsNotificacionesPosgradoApplicationTests.java
@@ -0,0 +1,13 @@
+package Postgrado.notificaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotificacionesPosgradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
