e381e40ddb57576d8a9cd7ea164a97910df3ac96
linderhp0607 <linderhidalgopinedo@gmail.com>
2026-01-15 18:31:33 -0500

    kafka consumer hecho

--- MsSimulacro/src/main/java/com/MsSimulacro/MsSimulacro/MsSimulacroApplication.java
+++ MsSimulacro/src/main/java/com/MsSimulacro/MsSimulacro/MsSimulacroApplication.java
@@ -3,7 +3,9 @@ package com.MsSimulacro.MsSimulacro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @EnableCaching
 @SpringBootApplication
 public class MsSimulacroApplication {
--- MsSimulacro/src/main/java/com/MsSimulacro/MsSimulacro/config/RedisCacheConfig.java
+++ MsSimulacro/src/main/java/com/MsSimulacro/MsSimulacro/config/RedisCacheConfig.java
@@ -4,10 +4,8 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -17,6 +15,7 @@ import java.time.Duration;
 @Configuration
 public class RedisCacheConfig {
 
+    @Deprecated(forRemoval=true)
     @Bean
     public RedisCacheConfiguration redisCacheConfiguration() {
 
