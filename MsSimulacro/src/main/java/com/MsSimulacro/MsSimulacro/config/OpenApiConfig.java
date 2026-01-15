406ec85248e8563f60b8a83c0814ad0da1301db3
linderhp0607 <linderhidalgopinedo@gmail.com>
2026-01-15 18:49:00 -0500

    corrección del swagger

--- MsSimulacro/src/main/java/com/MsSimulacro/MsSimulacro/config/OpenApiConfig.java
+++ MsSimulacro/src/main/java/com/MsSimulacro/MsSimulacro/config/OpenApiConfig.java
@@ -1,15 +1,17 @@
 package com.MsSimulacro.MsSimulacro.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
         info = @Info(
-                title = "Microservicio Simulacro",
+                title = "MsSimulacro API",
                 version = "1.0",
-                description = "API para gestión de Simulacros"
+                description = "Microservicio de Simulacros: Simulacro, SesionSimulacro, ResultadoSimulacro",
+                contact = @Contact(name = "Equipo TDUNU2025")
         )
 )
 public class OpenApiConfig {
